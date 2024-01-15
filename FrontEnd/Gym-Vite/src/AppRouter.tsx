041022f543985aec3bd51afae60d8ce4bd806724
volod_Dev <v.lysenko@student.utwente.nl>
2024-01-15 23:41:14 +0100

    Update v1.1 Patch FrontEnd

--- FrontEnd/Gym-Vite/src/AppRouter.tsx
+++ FrontEnd/Gym-Vite/src/AppRouter.tsx
@@ -1,6 +1,6 @@
 // AppRouter.tsx
 
-import React from 'react';
+import * as React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import RegistrationPage from './RegistrationPage';
