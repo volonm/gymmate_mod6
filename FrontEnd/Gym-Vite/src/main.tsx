ed2025605d7e7d72cb352a500cb5720366e6d4fa
volod_Dev <v.lysenko@student.utwente.nl>
2024-01-24 17:07:13 +0100

    Update V2 Final Version

--- FrontEnd/Gym-Vite/src/main.tsx
+++ FrontEnd/Gym-Vite/src/main.tsx
@@ -2,22 +2,32 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LoginPage from './LoginPage';
-import RegistrationPage from './RegistrationPage';
-import ChatRoom from "./ChatRoom.tsx";
-import ProfilePage from "./ProfilePage.tsx";
+import LoginPage from './pages/LoginPage.tsx';
+import RegistrationPage from './pages/RegistrationPage.tsx';
+import ChatRoom from "./pages/ChatRoom.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
-import TimetableContainer from "./TimetableContainer.tsx";
+import TimeAvailability from './pages/TimeAvailability.tsx';
+import TimetableContainer from './pages/TimetableContainer.tsx';
+import NextWeekAvailability from "./pages/NextWeekAvailability.tsx";
+import TimetableNextWeek from "./pages/TimetableNextWeek.tsx";
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-        <Routes>
-            <Route path="/register" element={<RegistrationPage />} />
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/chat" element={<ChatRoom />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/schedule" element={<TimetableContainer />} />
+      {/* <SchedulePage /> */}
+    <Routes>
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/availability" element={<TimeAvailability />} />
+        <Route path="/availability/nextweek" element={<NextWeekAvailability />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/" element={<TimetableContainer />} />
+        <Route path="/chat" element={<ChatRoom/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/next" element={<TimetableNextWeek/>} />
         </Routes>
     </Router>
   </React.StrictMode>,
+  document.getElementById('root')
 );
